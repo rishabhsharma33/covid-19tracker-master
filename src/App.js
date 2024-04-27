1eeeebb794c148b079b84c0aca64e9d16af7671d
Rishabh Sharma <137682358+rishabhsharma33@users.noreply.github.com>
2024-04-27 23:10:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react'
+import Statewise from './Components/Statewise/Statewise';
+// import './Components/Statewise/Statewise.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <>
+  <Statewise/>
+    </>
+  );
+}
+
+export default App;
